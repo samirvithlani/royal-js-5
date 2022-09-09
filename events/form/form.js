8ba9c4c44c8603720e48410722e0fda4d587a697
samirvithlani <samir.vithlani83955@gmail.com>
2022-09-09 17:46:03 +0530

    events1

--- events/form/form.js
+++ events/form/form.js
@@ -8,6 +8,10 @@ function submitForm(){
 
     //alert("Form submitted successfully");
     var name = document.getElementById("name").value
+    if(name == ""){
+        document.getElementById("name").style.border = "1px solid red"
+        document.getElementById("namevalidation").innerHTML = "Please enter your name"
+    }
     //console.log("Form submitted successfully");
     var gender = document.getElementById("gender").value
     var email = document.getElementById("email").value
@@ -28,4 +32,23 @@ function searchData(){
 
     var x = document.getElementById("search").value
     console.log("searching data",x);
+}
+
+function demo(){
+    console.log("demo function");
+}
+//validation
+//null ->
+
+
+
+function bluring(){
+
+    var email = document.getElementById("email").value
+    if(email == ""){
+
+        document.getElementById("emailvalidation").innerHTML = "Please enter your email"
+    }
+    
+
 }
---
+++ events/mouse/mouse2.js
@@ -0,0 +1,23 @@
+function zoom(){
+
+    document.getElementById("z").style.height = "400px",style.width = "400px";
+}
+
+function scrolling(){
+
+        alert("scrolling");
+}
+
+function loaded(){
+
+    alert("loaded");
+}
+function wheel(){
+
+    alert("wheel");
+}
+
+function up(event){
+
+console.log(event);
+}
